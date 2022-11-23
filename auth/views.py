aaa34e336cd2b14c21ec8e8f0d324cd48888c2cd
Gibril1 <gibrilissakaiddrisu@gmail.com>
2022-11-22 22:25:06 -0800

    code refactoring

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserSerializer,  RegisterSerializer
 
 # Create your views here.
+class UsersView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
